0de2533eae2f63372277988c0446d84b56d5d8db
yliff <yaakovliff@gmail.com>
2017-11-01 12:46:41 -0400

    HW3 attempt1

---
+++ HW3/test/TimerTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FakeClock implements ElapsedTimeClockInterface {
+
+    FakeClock(long ... vals){
+        returnValues = vals;
+    }
+    private int counter = 0;
+    private long returnValues[];
+
+    @Override
+    public long currentTimeMillis() {
+        return returnValues[counter++];
+    }
+}
+
+public class TimerTest {
+
+    @Test
+    void getElapsedTimeWhenStartStopShouldBe1003() {
+        ElapsedTimeClockInterface clock = new FakeClock(0, 52);
+        Timer t = new Timer( clock );
+
+        t.start();
+        t.stop();
+
+        assertEquals(52, t.getElapsedTime());
+    }
+
+}
